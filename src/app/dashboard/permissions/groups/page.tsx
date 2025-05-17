cd9530e6723ea785d249216d6fae16f142c6789e
Maksym Mukovoz <sgmaxim@gmail.com>
2025-05-17 15:33:16 +0200

    Add dashboard layout and update sign in page

---
+++ src/app/dashboard/permissions/groups/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Groups",
+  description: "Groups",
+}
+
+export default function SignIn() {
+  return (
+    <>Groups</>
+  )
+}
---
+++ src/app/dashboard/permissions/users/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Users",
+  description: "Users",
+}
+
+export default function SignIn() {
+  return (
+    <>Groups</>
+  )
+}
