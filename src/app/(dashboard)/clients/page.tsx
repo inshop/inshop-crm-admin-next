e61a5c647549b82cbb3182af80ad33dc39785013
Maksym Mukovoz <sgmaxim@gmail.com>
2025-05-19 01:08:41 +0200

    Add MUI DataGrid for client display and update package dependencies

--- src/app/(dashboard)/clients/page.tsx
+++ src/app/(dashboard)/clients/page.tsx
@@ -2,11 +2,54 @@
 
 import * as React from 'react'
 import {useClientsControllerFindAllQuery} from "@/lib/redux/features/clients";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import {useMemo} from "react";
 
 // export const metadata: Metadata = {
 //   title: "Clients",
 //   description: "Clients",
 // }
+
+const columns: GridColDef<(typeof rows)[number]>[] = [
+  {
+    field: 'id',
+    headerName: 'ID',
+    flex: 0.1,
+    minWidth: 100,
+    sortable: false,
+    disableColumnMenu: true,
+  },
+  {
+    field: 'name',
+    headerName: 'Name',
+    flex: 0.4,
+    minWidth: 200,
+    editable: true,
+    sortable: false,
+    disableColumnMenu: true,
+  },
+  {
+    field: 'email',
+    headerName: 'email',
+    flex: 0.4,
+    minWidth: 200,
+    editable: true,
+    sortable: false,
+    disableColumnMenu: true,
+  },
+  {
+    field: 'isActive',
+    headerName: 'Active',
+    type: 'boolean',
+    flex: 0.1,
+    minWidth: 100,
+    editable: true,
+    sortable: false,
+    disableColumnMenu: true,
+  }
+];
 
 export default function SignIn() {
   const { data, error, isLoading } = useClientsControllerFindAllQuery({
@@ -14,9 +57,41 @@ export default function SignIn() {
     skip: 0
   })
 
-  console.log('data', data, error, isLoading)
+  const rows = useMemo(() => {
+    if (data) {
+      return data[0].map((client: any) => ({
+        id: client.id,
+        name: client.name,
+        email: client.email,
+        isActive: client.isActive
+      }))
+    }
+
+    return []
+  }, [data])
+
+  // console.log('data', data, error, isLoading)
 
   return (
-    <>Clients</>
+    <>
+      <Typography variant="h2">Clients</Typography>
+
+      <Box sx={{ width: '100%', mt: 4 }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5, 10, 20]}
+          // checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </>
   )
 }
