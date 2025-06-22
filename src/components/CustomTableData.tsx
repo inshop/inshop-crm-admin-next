8d0dfef0ebc0cd8518b225131b8ad9fc8fc00c97
Maksym Mukovoz <sgmaxim@gmail.com>
2025-06-22 02:03:26 +0200

    Fix import statements in CustomTableData component for consistency

--- src/components/CustomTableData.tsx
+++ src/components/CustomTableData.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 import * as React from 'react'
-import {Alert, capitalize, CircularProgress} from "@mui/material";
+import {Alert, capitalize, CircularProgress} from '@mui/material';
 import {UseQuery} from '@reduxjs/toolkit/src/query/react/buildHooks'
-import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
-import Box from "@mui/material/Box";
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Box from '@mui/material/Box';
 
 interface CustomTableDataType {
   query: UseQuery<unknown>,
