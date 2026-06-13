6be991c229cd3503b09e64bc0b9c263f48b5a42b
Maksym Mukovoz <sgmaxim@gmail.com>
2026-06-13 15:35:37 +0200

    Fixed login fields styles

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -6,6 +6,29 @@ const theme = createTheme({
   typography: {
     fontFamily: "var(--font-roboto)",
   },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        input: ({ theme }) => {
+          const bg = (theme.vars ?? theme).palette.background.paper;
+          const color = (theme.vars ?? theme).palette.text.primary;
+          const autofillStyles = {
+            WebkitBoxShadow: `0 0 0 100px ${bg} inset`,
+            WebkitTextFillColor: color,
+            caretColor: color,
+            transition: "background-color 5000s ease-in-out 0s",
+          };
+
+          return {
+            "&:-webkit-autofill": autofillStyles,
+            "&:-webkit-autofill:hover": autofillStyles,
+            "&:-webkit-autofill:focus": autofillStyles,
+            "&:-webkit-autofill:active": autofillStyles,
+          };
+        },
+      },
+    },
+  },
 });
 
 export default theme;
